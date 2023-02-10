5d5aba930622d2643c8d7100af5234893bd29765
artisxiang <qiancaoxiang@163.com>
2023-02-10 13:10:49 +0800

    fix ！

--- lib/src/constant/screens.dart
+++ lib/src/constant/screens.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 /// 状态栏高度
-double kScreenTopPadding = WidgetsBinding.instance!.window.padding.top / 
-    WidgetsBinding.instance!.window.devicePixelRatio;
+double kScreenTopPadding = WidgetsBinding.instance.window.padding.top / 
+    WidgetsBinding.instance.window.devicePixelRatio;
 
 /// 水平方向的间距
 double kHorizontalPadding = 10.w;
